8607fda33a94f0edd4ebac373a06c7e91864a3b7
AM05307 <33053586+AM05307@users.noreply.github.com>
2018-01-19 18:19:34 +0900

    no message

--- test_one/src/test_one/test_class.java
+++ test_one/src/test_one/test_class.java
@@ -4,9 +4,8 @@ public class test_class {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("second commit");
 		// 너무 어렵다
-		//오늘의 작업은 게시판 완성
+		//오늘의 작업은 게시판 
 	}
 
 }
